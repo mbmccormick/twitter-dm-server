8574ed1727348de0b9ce4fe23bedf05678c66d27
Matt McCormick <mbmccormick@gmail.com>
2011-12-14 14:08:49 -0500

    Update POPServerConnection.java

--- POPServerConnection.java
+++ POPServerConnection.java
@@ -8,6 +8,7 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.DirectMessage;
+import twitter4j.Paging;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class POPServerConnection implements Runnable
